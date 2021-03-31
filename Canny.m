816775337f29f9b8db28ce006f18cab4c1f9f419
Maryam Karimi <mkarimif@uwo.ca>
2021-03-31 13:48:41 -0400

    Added Canny Edge and Harris Corner detectors

--- Canny.m
+++ Canny.m
@@ -1,30 +1,31 @@
-% image = imread('image1.jfif');
-% image = rgb2gray(image);
-% imshow(canny(img, 1.4, 2.4, 2.8));
 function [result_image] = canny(image, sigma, low_threshold, high_threshold)
+    
+    % colour to grayscale
+    image = double(rgb2gray(image));
+
     [image_height, image_width] = size(image);
     
-    N = 2;
-    x = -N : N;
-    y = -transpose(x);
+    n = 2;
+    x = -n : n;
+    y = transpose(x);
 
     % Two 1D Gaussian Filters with standard deviation sigma
-    Gx = exp(-((x.^2) / (2 * sigma^2))) / (sqrt(2 * pi) * sigma);
-    Gy = exp(-((y.^2) / (2 * sigma^2))) / (sqrt(2 * pi) * sigma);
+    Gx = exp(-((x.^2) / (2 * sigma^2)));
+    Gy = exp(-((y.^2) / (2 * sigma^2)));
 
     % Convoluting image with the two Gaussian filters
     % Apply 1D Gaussian in X direction
-    blurred_image = conv2(image, Gx, 'same');
+    filtered_image = convolve(image, Gx);
     % Apply 1D Gaussian in Y direction
-    blurred_image = conv2(blurred_image, Gy, 'same');
+    filtered_image = convolve(filtered_image, Gy);
 
     % Step 1: Compute x and y derivatives of image
     % Could also use gradient(Gx) and gradient(Gy)
-    Hx = -(2^(1/2) .* x .* exp(-x.^2 / (2 * sigma^2))) / (2 * pi^(1/2) * sigma^3);
-    Hy = -(2^(1/2) .* y .* exp(-y.^2 / (2 * sigma^2))) / (2 * pi^(1/2) * sigma^3);
+    Hx = -x / sigma^2 .* exp(-x.^2 / (2 * sigma^2));
+    Hy = -y / sigma^2 .* exp(-y.^2 / (2 * sigma^2));
 
-    image_x = conv2(blurred_image, Hx, 'same');
-    image_y = conv2(blurred_image, Hy, 'same'); 
+    image_x = convolve(filtered_image, Hx);
+    image_y = convolve(filtered_image, Hy); 
 
     % Step 2: Compute magnitude and direction of every pixel
     magnitude = sqrt(image_x.^2 + image_y.^2);
@@ -38,6 +39,26 @@ function [result_image] = canny(image, sigma, low_threshold, high_threshold)
 
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Helper Functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function [result] = convolve(image, kernel)
+        % initialize the output result
+        result = zeros(size(image));
+
+        % find the width and heights
+        [kernel_height, kernel_width] = size(kernel);
+        
+        % pad the image with zeros
+        padded_image = padarray(image, [floor(kernel_height / 2) floor(kernel_width / 2)]);
+        kernel = reshape(transpose(flipdim(flipdim(kernel, 1), 2)), [], 1);
+
+        for i = 1 : image_height
+            for j = 1 : image_width
+                vectpadded_image = reshape(transpose(padded_image(i : i + kernel_height - 1, j : j + kernel_width - 1)), 1, []);
+                result(i, j) = vectpadded_image * kernel;
+            end
+        end
+    end  
+  
+
     function result_image = thin_edges(image_height, image_width, magnitude, direction)
         
         result_image = zeros(image_height, image_width);
@@ -69,7 +90,7 @@ function [result_image] = canny(image, sigma, low_threshold, high_threshold)
                     after = magnitude(i + 1, j + 1);
                 end
 
-                if pixel_magnitude > before && pixel_magnitude > after
+                if pixel_magnitude >= before && pixel_magnitude >= after
                     result_image(i, j) = pixel_magnitude;
                 end
             end
@@ -86,7 +107,7 @@ function [result_image] = canny(image, sigma, low_threshold, high_threshold)
                     result_image(i, j) = 0;
                 
                 elseif (image(i, j) > high_threshold)
-                    result_image(i, j) = 255;
+                    result_image(i, j) = 1;
                 
                 elseif (image(i, j) > low_threshold)   
                     if ((image(i + 1, j) > high_threshold) || ...
@@ -97,7 +118,7 @@ function [result_image] = canny(image, sigma, low_threshold, high_threshold)
                         (image(i - 1, j - 1) > high_threshold) || ...
                         (image(i + 1, j - 1) > high_threshold) || ...
                         (image(i - 1, j + 1) > high_threshold))
-                        result_image(i, j) = 255;
+                        result_image(i, j) = 1;
                     end
                 end
             end
